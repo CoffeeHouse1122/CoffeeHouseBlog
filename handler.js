ea1175fc71a824d28949740bc8c779a8d627a43d
qzp076 <1158165115@qq.com>
2018-03-25 15:36:34 +0800

    3.25-1

---
+++ handler.js
@@ -0,0 +1,10 @@
+//处理首页的函数
+// index.js
+//处理用户的函数
+// user.js
+//处理文章的函数
+// article.js
+//处理商品的函数
+// product.js
+//出礼分类的函数
+// category.js
---
+++ router.js
@@ -0,0 +1,39 @@
+//0.加载express
+const express = require("express");
+//加载所有的处理函数模块
+const index = require("./controllers/index");
+const user = require("./controllers/user");
+const topic = require("./controllers/topic");
+//1.调用express.Router()创建一个路由实例
+const router = express.Router();
+//2.配置路由规则
+// 首页路由
+router
+    .get("/",index.showIndex);
+//用户路由
+router
+    .get("/signin",user.showSignin)
+    .post("/signin",user.signin)
+    .get("/signup", user.showSignup)
+    .post("/signup", user.signup)
+    .post("/signout", user.signout)
+//话题相关
+router
+    .get('/topic/create', topic.showCreate)
+    .post('/topic/create', topic.create)
+    .get('/topic/:topicID', topic.show)
+    .get('/topic/:topicID/edit', topic.showEdit)
+    .post('/topic/:topicID/edit', topic.edit)
+    .post('/topic/:topicID/delete', topic.delete)
+    
+
+
+    
+    
+
+
+
+
+
+//3.导出路由对象
+module.exports = router;
